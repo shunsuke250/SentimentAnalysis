373b07b26e16438faa123fc4457f02dceef84307
Shunsuke Soeyama <92722651+shunsuke250@users.noreply.github.com>
2022-12-16 14:41:59 +0900

    Delete streamlit/venv directory

--- streamlit/venv/bin/mlask-admin.py
+++
@@ -1,6 +0,0 @@
-#!/Users/soeyamashunsuke/Desktop/streamlit/venv/bin/python3
-
-from mlask import management
-
-if __name__ == "__main__":
-	management.execute()
--- streamlit/venv/lib/python3.9/site-packages/mlask/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from .mlask import MLAsk
-
-VERSION = (0, 3, 2)
-__version__ = '0.3.2'
-__all__ = ['MLAsk']
